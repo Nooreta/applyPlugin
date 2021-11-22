78d2ff67c13010a60ab194784510310a77e5a115
noorjallad99 <48832632+noorjallad99@users.noreply.github.com>
2021-11-22 19:12:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.element', {
+    strings: ["Front-end-development", "Back-end-development"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop:Infinity,
+    cursorChar: '..'
+   
+  
+  });
